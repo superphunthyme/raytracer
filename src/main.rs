1a37aa4dd103a2fa3796c5f40aa837bfcd01298e
superphunthyme <igollish@gmail.com>
2019-02-11 20:50:01 -0500

    Inital commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+
+    let x_res = 200;
+    let y_res = 100;
+    let colour_range = 255;
+    
+    // Rewrite as create_ppm
+    println!("P3\n{} {}\n{}\n", x_res, y_res, colour_range);
+
+    for j in (0..y_res).rev() {
+        for i in 0..x_res {
+            let r = i as f32 / x_res as f32 * 255.99;
+            let g = j as f32 / y_res as f32 * 255.99;
+            let b: f32 = 0.2 * 255.99;
+
+            let ir = r.floor();
+            let ig = g.floor();
+            let ib = b.floor();
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+}
