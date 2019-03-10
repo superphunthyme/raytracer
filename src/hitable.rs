ffe74e7a709954e07c663f0c6e93f97ce2045809
superphunthyme <igollish@gmail.com>
2019-03-10 15:32:45 -0400

    Multiple hitable objects

--- src/hitable.rs
+++ src/hitable.rs
@@ -13,3 +13,36 @@ pub struct HitRecord {
 pub trait Hitable {
     fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord>;
 }
+
+pub struct HitableList {
+    hitables: Vec<Box<dyn Hitable>>
+}
+
+impl HitableList {
+    pub fn new() -> HitableList {
+        HitableList {
+            hitables: Vec::new(),
+        }
+    }
+
+    pub fn add<T: Hitable + 'static>(&mut self, item: T) {
+        self.hitables.push(Box::new(item));
+    }
+}
+
+impl Hitable for HitableList {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let mut temp_rec: Option<HitRecord> = None;
+        let mut closest_so_far = t_max;
+        for hitable in self.hitables.iter() {
+            match hitable.hit(r, t_min, closest_so_far) {
+                Some(hr) => {
+                    closest_so_far = hr.t;
+                    temp_rec = Some(hr);
+                },
+                None => ()
+            }
+        }
+        temp_rec
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod vec3;
 use crate::vec3::Vector3;
 use crate::camera::Camera;
 use crate::hitable::Hitable;
+use crate::hitable::HitableList;
 use crate::material::Material;
 use crate::ray::Ray;
 use crate::sphere::Sphere;
@@ -45,7 +46,7 @@ fn main() {
 
     let x_res = 200;
     let y_res = 100;
-    let num_samples = 200;
+    let num_samples = 100;
     let colour_range = 255;
     
     // Rewrite as create_ppm
@@ -57,19 +58,28 @@ fn main() {
     let dist_to_focus = (lookfrom - lookat).length();
     let aperture = 0.0;
     let cam = Camera::new(lookfrom, lookat, vup, 90.0, x_res as f32 / y_res as f32, aperture, dist_to_focus);
+
     let sphere1 = Sphere::new(Vector3::new(0.0, 0.0, -1.0), 0.5, Material::Metal{ albedo: Vector3::new(1.0, 0.0, 0.0), fuzz: 0.0});
+    let sphere2 = Sphere::new(Vector3::new(0.0, -100.5, -1.0), 100.0, Material::Lambertian{ albedo: Vector3::new(0.8, 0.8, 0.0)});
+    let sphere3 = Sphere::new(Vector3::new(1.0, 0.0, -1.0), 0.5, Material::Lambertian{ albedo: Vector3::new(0.0, 0.0, 1.0)});
+    let mut hitable_list = HitableList::new();
+    hitable_list.add(sphere1);
+    hitable_list.add(sphere2);
+    hitable_list.add(sphere3);
+
     for j in (0..y_res).rev() {
         for i in 0..x_res {
             let mut col = Vector3::new(0.0, 0.0, 0.0);
-            for s in 0..num_samples {
+            for _s in 0..num_samples {
                 let u_rand = rand::thread_rng().gen_range(0.0, 1.0);
                 let v_rand = rand::thread_rng().gen_range(0.0, 1.0);
                 let u = (i as f32 + u_rand) / x_res as f32;
                 let v = (j as f32 + v_rand) / y_res as f32;
                 let ray = cam.get_ray(u, v);
-                col = col + color(&ray, &sphere1, 0);
+                col = col + color(&ray, &hitable_list, 0);
             }
             col = col / num_samples as f32;
+            col = Vector3::new(col.r().sqrt(), col.g().sqrt(), col.b().sqrt());
 
             let out_colour = Vector3::new((255.99 * col.r()).floor(), (255.99 * col.g()).floor(), (255.99 * col.b()).floor());
             println!("{}", out_colour);
